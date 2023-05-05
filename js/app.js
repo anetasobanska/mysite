c1621bafcc1a1e4a24a743b496b3d1f488f23847
anetasobanska <sannettka@gmail.com>
2023-05-05 16:16:03 +0200

    added animationst to the projects section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-        gsap.from('.list-item', {
+        gsap.from(
+          '.list-item', {
           opacity: 0,
           scale: 0,
           ease: 'back',
@@ -15,9 +16,89 @@ document.addEventListener('DOMContentLoaded', function () {
           1.8,
           {
             x: 1900,
-            duration: 0.8,
+            duration: 1,
             ease: 'back',
             stagger: 0.1,
           }
         )
+
+        gsap.from(
+          '.project-description.one',
+          {
+            x: 1900,
+            duration: 1.5,
+            ease: 'bounce',
+            scrollTrigger: {
+              trigger: '.project-description.one',
+              start: 'top center',
+            }
+          }
+        )
+
+        gsap.from(
+          '.image_wrapper.one',
+          {
+            x: -1900,
+            duration: 1.5,
+            ease: 'bounce',
+            scrollTrigger: {
+              trigger: '.project-description.one',
+              start: 'top center',
+            }
+          }
+        )
+
+        gsap.from(
+          '.project-description.two',
+          {
+            x: -1900,
+            duration: 1.5,
+            ease: 'bounce',
+            scrollTrigger: {
+              trigger: '.project-description.two',
+              start: 'top center',
+            }
+          }
+        )
+
+        gsap.from(
+          '.image_wrapper.two',
+          {
+            x: 1900,
+            duration: 1.5,
+            ease: 'bounce',
+            scrollTrigger: {
+              trigger: '.project-description.two',
+              start: 'top center',
+            }
+          }
+        )
+
+        gsap.from(
+          '.project-description.three',
+          {
+            x: 1900,
+            duration: 1.5,
+            ease: 'bounce',
+            scrollTrigger: {
+              trigger: '.project-description.three',
+              start: 'top center',
+            }
+          }
+        )
+
+        gsap.from(
+          '.image_wrapper.three',
+          {
+            x: -1900,
+            duration: 1.5,
+            ease: 'bounce',
+            scrollTrigger: {
+              trigger: '.project-description.three',
+              start: 'top center',
+            }
+          }
+        )
+
+        
 })
